be1f270a11e55d58c9ed6f850f4bce03e9baf094
pgianferro <pablogianferro@gmail.com>
2025-06-08 16:06:57 -0300

    mapping update in refund all function

--- Subasta.sol
+++ Subasta.sol
@@ -3,39 +3,42 @@ pragma solidity >0.8.0;
 
 contract Subasta {
 
-//Variables de estado
+//VARIABLES DE ESTADO
 uint public highestBid;
 address public highestBidder;
 uint256 public startTime;
 uint256 public stopTime;
 address payable public owner;
-mapping(address => uint) bids;
+mapping(address => uint) bids; //mapping de oferentes y montos
 
-//Variables de estado para el listado de oferentes y ofertas
+//Para el listado de oferentes y ofertas
 struct BidInfo {
-    address bidder;
-    uint256 amount;
-    uint256 time;
-    bool refunded;
+    address bidder; //oferente
+    uint256 amount; //monto ofertado
+    uint256 time; //momento de la oferta
+    bool refunded; //indicador de reembolso
 }
 
-//Eventos
-event NewBid(address indexed bidder, uint amount);
-event AuctionEnded();
 
+//EVENTOS
+event NewBid(address indexed bidder, uint amount); //indica que se realizó una oferta
+event AuctionEnded(); // indica el final de la Subasta
+
+
+//ARRAYS
 //Array para contener a los oferentes
 BidInfo[] public bidHistory;
 
-//Array
-
-//Constructor para inicializar las variables de estado
+//CONSTRUCTOR
+//Para inicializar las variables de estado
 constructor() {
     startTime = block.timestamp;
     stopTime = startTime + 5 minutes;
     owner = payable(msg.sender);
 }
 
-//Modificadores
+
+//MODIFICADORES
 //Solo permite que el owner llame a la función
 modifier onlyOwner() {
     require(msg.sender == owner, "Only the owner can call this function.");
@@ -54,9 +57,10 @@ modifier auctionFinalized() {
     _;
 }
 
+//FUNCIONES
+
 //Función para registrar una subasta
 function bid() external payable auctionActive {
-    //require(block.timestamp >= startTime && block.timestamp <= stopTime, "Bid is not active");
     require(msg.value > 0, "You must send some ETH");
     require(msg.value * 100 >= highestBid * 105, "Bid must be at least 5% higher");
 
@@ -71,25 +75,31 @@ function bid() external payable auctionActive {
     // Registro de la oferta (acumulativo, por si luego usamos reembolsos)
     bids[msg.sender] += msg.value;
 
-    // Actualizamos solo si es la mejor oferta individual
+    // Actualizamos la mejor oferta individual
     highestBid = msg.value;
     highestBidder = msg.sender;
 
-    //Lo agregamos al historial de oferentes para consulta
+    //Lo agregamos al historial de oferentes para consultas y reembolsos
     bidHistory.push(BidInfo(msg.sender, msg.value, block.timestamp, false));
 }
+
+
 
 //Función para leer apuestas por contrato
 function getBidOf(address bidder) external view returns (uint) {
     return bids[bidder];
 }
 
+
+
 //Función para mostrar quien va ganando
 function showCurrentWinner() external view returns (address, uint) {
     return (highestBidder, highestBid);
 }
 
-//Mostrar oferentes y sus ofertas
+
+
+//Función para mostrar oferentes y sus ofertas
 function showBids() external view returns (BidInfo[] memory) {
     return bidHistory;
 }
@@ -99,7 +109,7 @@ function showBids() external view returns (BidInfo[] memory) {
 function partialRefund() external auctionActive {
     uint256 count = 0;
 
-    // 1. Contar cuántas ofertas activas (no reembolsadas) tiene este bidder
+    // 1. Contar cuántas ofertas activas (no reembolsadas) tiene este oferente
     for (uint i = 0; i < bidHistory.length; i++) {
         if (bidHistory[i].bidder == msg.sender && !bidHistory[i].refunded) {
             count++;
@@ -116,7 +126,7 @@ function partialRefund() external auctionActive {
         if (bidHistory[i].bidder == msg.sender && !bidHistory[i].refunded) {
             counter++;
             if (counter < count) {
-                refundAmount += bidHistory[i].amount;
+                refundAmount += (bidHistory[i].amount * 98) / 100;
                 bidHistory[i].refunded = true;
             }
         }
@@ -128,28 +138,33 @@ function partialRefund() external auctionActive {
     (bool sent, ) = payable(msg.sender).call{value: refundAmount}("");
     require(sent, "Refund failed");
 
-    // 4. Restar del total acumulado del mapping
+    // 4. Restar del total acumulado del mapping para actualizarlo
     bids[msg.sender] -= refundAmount;
 }
+
+
 
 //Función para mostrar ganador final
 function showFinalWinner() external view auctionFinalized returns (address, uint) {
     return (highestBidder, highestBid);
 }
 
+
+
 //Función para devolver las ofertas a los no ganadores
 function refundAll() external payable onlyOwner auctionFinalized { 
     for(uint i = 0; i < bidHistory.length; ++i) {
-        if(bidHistory[i].amount != highestBid && bidHistory[i].refunded == false) {
-            uint refundAmount = (bidHistory[i].amount) * 98 / 100;
+        if(bidHistory[i].amount != highestBid && bidHistory[i].refunded == false) { //chequeamos todas las ofertas en estado refunded false y en caso de que hubiera ofertas del ganador inferiores a la que lo dio ganador también las devolvemos
+            uint refundAmount = (bidHistory[i].amount) * 98 / 100; //calculamos el reembolso menos la comisión
             //payable(bidHistory[i].bidder).transfer(refundAmount);
-            (bool sent, ) = payable(bidHistory[i].bidder).call{value: refundAmount}("");
+            (bool sent, ) = payable(bidHistory[i].bidder).call{value: refundAmount}(""); //reembolsamos al oferente
             require(sent, "Refund failed");
             
-            bidHistory[i].refunded = true;
+            bidHistory[i].refunded = true; //pasamos a true los reembolsos, todos incluso del ganador excepto su última oferta
+            bids[bidHistory[i].bidder] -= refundAmount; //actualizamos el mapping descontando los reembolsos efectuados
         }
     }
-    emit AuctionEnded();
+    emit AuctionEnded(); //emitimos el evento de finalización de la Subasta
 }
 
 //Funcion para saber la hora actual, solo para testeo
